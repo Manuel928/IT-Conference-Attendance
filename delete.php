d43f096e775cfca58bf74d1c9dbdb9edbd089f65
Emmanuel Oluwatosin Alao <Afrikareportersng@gmail.com>
2023-07-28 19:20:41 +0100

    corrected the delete route

--- delete.php
+++ delete.php
@@ -10,7 +10,7 @@ if (!isset($_GET['id'])) {
     $result = $crud->deleteNote($id);
 
     if($result) {
-        header('Location: index.php');
+        header('Location: viewrecords.php');
     } else {
         include 'includes/errormessage.php';
     }
